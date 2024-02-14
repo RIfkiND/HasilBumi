3da9e40a233be97794839a6a38d46c3db083a84c
ajiw <ajistwn301@gmail.com>
2024-02-14 16:57:19 +0700

    saya telah membuat beberapa file

---
+++ resources/js/Pages/login-regis.vue
@@ -0,0 +1,10 @@
+<script setup>
+   import '@fortawesome/fontawesome-free/css/all.css'
+    import 'material-design-icons-iconfont/dist/material-design-icons.css'
+    import { Link } from '@inertiajs/vue3'
+</script>
+
+<template>
+    <h1>Hello, world!</h1>
+    <a :href="route('home')">Pergi ke halaman home</a>
+</template>
