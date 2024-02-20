77581624a5643ad61daca4c4dc56d27932babbfd
ajiw <ajistwn301@gmail.com>
2024-02-20 10:01:10 +0700

    edit file register.vue

--- resources/js/Pages/User/Auth/Register.vue
+++ resources/js/Pages/User/Auth/Register.vue
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <script setup>
    import '@fortawesome/fontawesome-free/css/all.css'
     import 'material-design-icons-iconfont/dist/material-design-icons.css'
@@ -8,12 +7,4 @@
 
 <template>
     <h1>hello world</h1>
-=======
-<script>
-</script>
-<template>
-    <div>
-
-    </div>
->>>>>>> b4d64cda187eac0b872a6d3d80914b258e5eb01b
 </template>
