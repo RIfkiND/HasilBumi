fd1f50700b13ad3389820ba201181145ca763aa1
RIfkiND <122434422+RIfkiND@users.noreply.github.com>
2024-03-07 21:57:12 +0700

    update

---
+++ resources/js/Pages/Seller/Testing/Filter/FileterResult.vue
@@ -0,0 +1,17 @@
+<!-- resources/js/Pages/Products/Index.vue -->
+
+<template>
+    <div>
+        <h1>Products Belonging to {{ category }}</h1>
+        <ul>
+            <li v-for="product in products" :key="product.id">{{ product.name }}</li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    category: String,
+    products: Array
+});
+</script>
---
+++ resources/js/Pages/Seller/Testing/Filter/filterbutton.vue
@@ -0,0 +1,13 @@
+<template>
+    <button @click="filterByCategory('wood')">Filter by Wood</button>
+</template>
+
+<script setup>
+import { usePage } from "@inertiajs/vue3";
+import { router } from "@inertiajs/vue3";
+
+const filterByCategory = (category) => {
+    router.visit(route('',{category}));
+
+};
+</script>
