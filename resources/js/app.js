d388be2ffdcc0dc31cbdf0d79d770655388678d7
Rifki <rifkinauvaldzaki08@gail.com>
2024-04-30 11:17:40 +0700

    check

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,12 +2,18 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import VueSweetaler2 from 'vue-sweetalert2';
-import '@fortawesome/fontawesome-free/css/all.css'
-import '@fortawesome/fontawesome-free/js/all.js'
+import { createVuetify } from 'vuetify'
 // import { plugin as formkitPlugin, defaultConfig } from '@formkit/vue';
 // import { createMultiStepPlugin } from '@formkit/addons';
-//vuetify
-import vuetify from './plugins/vuetify';
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
